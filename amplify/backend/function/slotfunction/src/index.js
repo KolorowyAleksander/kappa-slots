314895e2708281c6e94e95b4a649e7cb238f9cf4
kolorowyaleksander <kolorowyaleksander@users.noreply.github.com>
2019-10-13 14:10:36 +0200

    M

--- amplify/backend/function/slotfunction/src/index.js
+++ amplify/backend/function/slotfunction/src/index.js
@@ -6,8 +6,32 @@ var region = process.env.REGION
 Amplify Params - DO NOT EDIT */
 
 exports.handler = function (event, context) { //eslint-disable-line
-  console.log(`value1 = ${event.key1}`);
-  console.log(`value2 = ${event.key2}`);
-  console.log(`value3 = ${event.key3}`);
-  context.done(null, 'Hello World'); // SUCCESS with message
+  console.log(`value1 = ${event.user}`);
+
+  const win = getRandomInt(1);
+  const possibleResults = ['mellon', 'warek', 'cherry', 'dollar'];
+
+  const result = (win)
+    ? {
+      first: 'kappa',
+      second: 'kappa',
+      third: 'kappa',
+      winner: true,
+    }
+    : {
+      first: getRandomElement(possibleResults),
+      second: getRandomElement(possibleResults),
+      third: getRandomElement(possibleResults),
+      winner: false,
+    };
+
+  context.done(null, result);
 };
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,6 +1,15 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const runSlotmachine = `mutation RunSlotmachine($user: String!) {
+  runSlotmachine(user: $user) {
+    first
+    second
+    third
+    winner
+  }
+}
+`;
 export const createMyType = `mutation CreateMyType($input: CreateMyTypeInput!) {
   createMyType(input: $input) {
     id
