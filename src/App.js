7c5eaf6c51daee909ffc1c69563074574cda0596
kolorowyaleksander <kolorowyaleksander@users.noreply.github.com>
2019-10-13 13:33:04 +0200

    M

--- src/App.js
+++ src/App.js
@@ -1,14 +1,117 @@
-import { Component } from 'react';
-import Amplify from 'aws-amplify';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Amplify, { Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
 import awsconfig from './aws-exports';
-import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+import { createMyType } from './graphql/mutations';
 
 Amplify.configure(awsconfig);
 
-class App extends Component {}
 
-const authConfig = {
+function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">MAIN</Link></li>
+                    </ul>
+                    <ul>
+                        <li><Link to="/leaderboard">SLOT</Link></li>
+                    </ul>
+                    <ul>
+                        <li><Link to="/user">LEADERBOARD</Link></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/leaderboard">
+                        <Leaderboard />
+                    </Route>
+                    <Route path="/slot">
+                        <Slot />
+                    </Route>
+                    <Route path="/">
+                        <Homo />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
 
+
+async function addElement () {
+    const elementDetails = {
+        input: {
+            title: 'Party tonight!',
+            content: 'Amplify CLI rocks!'
+        }
+    };
+
+    const result = await API.graphql(graphqlOperation(createMyType, elementDetails));
+    alert(JSON.stringify(result));
+};
+  
+
+function Homo() {
+    return (
+        <>
+            <h1>VERY UGLY HEADING 1</h1>
+            <button onClick={addElement}>DO SOMETHING</button>
+        </>
+    );
+}
+
+
+function Slot() {
+    return <h2>SLOT</h2>;
+}
+
+
+function Leaderboard() {
+    return (
+        <ol>
+            <li>SAMWON </li>
+            <li>SOMEWON ELS</li>
+            <li>uganda numba 3</li>
+        </ol>
+    );
+}
+
+
+const usernameAttributes = 'USERNAME';
+const signUpConfig = {
+    header: 'Sign Up For The Game',
+    hideAllDefaults: true,
+    defaultCountryCode: '48',
+    signUpFields: [
+        {
+            label: 'USERNAME',
+            key: 'username',
+            required: true,
+            displayOrder: 1,
+            type: 'string'
+        },
+        {
+            label: 'PASSWRD',
+            key: 'password',
+            required: true,
+            displayOrder: 2,
+            type: 'password'
+        },
+        {
+            label: 'EMAIL',
+            key: 'email',
+            required: true,
+            displayOrder: 3,
+            type: 'string'
+        }
+    ]
 };
 
-export default withAuthenticator(App, true);
+
+export default withAuthenticator(App, { 
+    signUpConfig,
+    usernameAttributes,
+});
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMyType = `subscription OnCreateMyType {
+  onCreateMyType {
+    id
+    title
+    content
+  }
+}
+`;
+export const onUpdateMyType = `subscription OnUpdateMyType {
+  onUpdateMyType {
+    id
+    title
+    content
+  }
+}
+`;
+export const onDeleteMyType = `subscription OnDeleteMyType {
+  onDeleteMyType {
+    id
+    title
+    content
+  }
+}
+`;
